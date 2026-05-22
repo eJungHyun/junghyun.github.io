58cc80048052ead735d8a7a7ff20201051e160fe
eJungHyun <junghyun.j.lee@gmail.com>
2026-05-22 13:44:19 +0000

    Deploying to gh-pages from @ eJungHyun/ejunghyun.github.io@c6ae00a5871f734b1bdcd10b8ebcf85036e3019c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "\* Equal Contribution, † Co-corresponding",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
